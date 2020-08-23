abc6af2654911855489649fca41f8cd5c6051751
choz <vadz77@hotmail.com>
2020-08-23 08:50:44 -0700

    Refactor code

--- mangascrapers/utils.py
+++ mangascrapers/utils.py
@@ -6,19 +6,19 @@ class utils():
 
     @staticmethod
     def save_image_from_url(url, filename):
-        filename = "{}/{}".format(utils.MANGADIR, filename)
-        dirname = os.path.dirname(filename)
+        local_filename = "{}/{}".format(utils.MANGADIR, filename)
+        dirname = os.path.dirname(local_filename)
 
         if not os.path.exists(dirname):
             os.makedirs(dirname)
 
-        if not os.path.exists(filename):
-            print("Saving image from url to: " + filename)
+        if not os.path.exists(local_filename):
+            print("Saving image from url to: " + local_filename)
 
             opener = urllib.request.build_opener()
             opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36')]
             urllib.request.install_opener(opener)
-            urllib.request.urlretrieve(url, filename)
+            urllib.request.urlretrieve(url, local_filename)
 
         return True
 
